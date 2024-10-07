11372f80c04f07beb95e9db1ae2998ae783f5c91
Chengcheng Hu <Chengchenghu0728@gmail.com>
2024-10-07 08:03:46 +0800

    ➕ 更新依赖文件

--- app/services/face_service.py
+++ app/services/face_service.py
@@ -1,25 +1,25 @@
 import sys
+from io import BytesIO
 from typing import List, Tuple
 from ultralytics import YOLO
-
-import cv2
 import numpy as np
 from werkzeug.datastructures import FileStorage
-
+from PIL import Image
 from dataclasses import dataclass
 
 
 @dataclass
 class User:
     faceEmbedding: List[float]
-    xyxy: List[Tuple[int, int, int, int]]
+    xyxy: Tuple[int, int, int, int]
 
 
 model = YOLO('app/static/model/best.pt')
 
 
 def calculate_embedding(face_img: FileStorage) -> List[User]:
-    img_source = cv2.imdecode(np.frombuffer(face_img.read(), np.uint8), cv2.IMREAD_COLOR)
+    img = Image.open(BytesIO(face_img.read())).convert('RGB')
+    img_source = np.array(img)
     results = model(img_source)
     faces: List[User] = []
     for result in results:
@@ -28,7 +28,7 @@ def calculate_embedding(face_img: FileStorage) -> List[User]:
                 x1, y1, x2, y2 = box.xyxy[0].int().tolist()
                 new_user = User(
                     faceEmbedding=[0.1, 0.2, 0.3, 0.4],
-                    xyxy=[x1, y1, x2 - x1, y2 - y1]
+                    xyxy=(x1, y1, x2 - x1, y2 - y1)
                 )
                 faces.append(new_user)
     return faces
